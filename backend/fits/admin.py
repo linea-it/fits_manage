9b3ec2c96b319d9fcea6cc8d76ef4a02217f1524
DeivedPatterson <deived.william@live.com>
2018-10-15 17:43:06 -0300

    f

--- backend/fits/admin.py
+++ backend/fits/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
 
-from .models import Header
+from .models import Header, Exposure
+
+@admin.register(Exposure)
+class ExposureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'filename', 'file_path', 'file_size')
+    search_fields = ()
+
 @admin.register(Header)
 class HeaderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'filename', )
+    list_display = ('id', 'archive', 'ra', 'dec')
     search_fields = ()
