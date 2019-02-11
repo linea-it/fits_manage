9a064e59d257dc44671ab5f880db336ac614e6ac
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2019-02-11 14:37:59 -0200

    Django Filters + Graphene has been configured

--- backend/lna/schema.py
+++ backend/lna/schema.py
@@ -1,25 +1,58 @@
 
 import graphene
 
+from graphene import Node
+from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 
 from lna.models import Exposure, Header
 
-class ExposureType(DjangoObjectType):
+
+class ExposureNode(DjangoObjectType):
     class Meta:
         model = Exposure
+        interfaces = (Node, )
+        filter_fields = {
+            'filename': ['iexact', 'icontains', 'istartswith'],
+            'target': ['iexact', 'icontains', 'istartswith']
+        }
 
-class HeaderType(DjangoObjectType):
-    class Meta:
-        model = Header
+
+# class HeaderType(DjangoObjectType):
+#     class Meta:
+#         model = Header
+
 
 class Query(object):
 
-    all_exposures = graphene.List(ExposureType)
-    all_headers = graphene.List(HeaderType)
+    exposure = Node.Field(ExposureNode)
+    all_exposures = DjangoFilterConnectionField(ExposureNode)
 
-    def resolve_all_exposures(self, info, **kwargs):
-        return Exposure.objects.all()
+    # def resolve_all_exposures(self, info, **kwargs):
+    #     return Exposure.objects.all()
 
-    def resolve_all_headers(self, info, **kwargs):
-        return Header.objects.select_related('exposure').all()
+    # def resolve_all_headers(self, info, **kwargs):
+    #     return Header.objects.select_related('exposure').all()
+
+
+#  Exemplo de querys 
+# query all_exposures {
+# 	allExposures(target_Icontains: "mali", first:2) {
+# 	  edges {
+# 	    node {
+# 	      id
+#         filename
+#         target
+# 	    }
+# 	  }
+# 	} 
+# }
+
+
+# query get_exposure_by_id {
+#   exposure (id:"RXhwb3N1cmVOb2RlOjgy"){
+#     id
+#     filename
+#     target
+#   }
+# }
